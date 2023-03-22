fa3bba5d2955c0c96d223e07ae5f3f1888f80380
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-03-21 20:06:04 -0700

    added in conditional statements to see if the user guessed the current number

--- app.js
+++ app.js
@@ -19,3 +19,30 @@ const randomNum = (x,y) => {
     max = Math.floor(y);
      return Math.floor((Math.random()* (max-min+1) + min))
 }
+const ranNumber = randomNum(parseInt(lowerBound), parseInt(upperBound));
+const form = document.querySelector("#guess");
+const guessh2 = document.querySelector("#currentG");
+
+let guess = 0;
+
+
+const input = document.querySelector("input");
+
+
+   form.addEventListener('submit', (x)=> {
+    x.preventDefault();
+    guess = parseInt(input.value);
+    input.value = "";
+    if(ranNumber > guess){
+        guessh2.innerText = `current guess is: ${guess}; number is too low`
+    }
+    else if (ranNumber < guess){
+        guessh2.innerText = `current guess is: ${guess}; number is too high`
+    }
+    else {
+        guessh2.innerText = `current guess is: ${guess}; number was ${guess}, you got it!`
+    } 
+    
+
+})
+
