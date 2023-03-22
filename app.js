6e50b23565fc8fda3db603ecdcb3fdf58e234ad9
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-03-21 19:36:29 -0700

    added random number function, comments, and h2 css

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+//prompts user to enter the lowest number
 const lowerBound =  prompt("what is the lowest number?");
-const upperBound = prompt("What is the highest number?")
+//prompts user to enter the highest number
+const upperBound = prompt("What is the highest number?");
 
+//variable used to change the value of the first h2
+const lBound = document.querySelector("#lowerBound");
+//variable used to change the value of the second h2
+const uBound = document.querySelector("#upperBound");
+
+//changes the text of the first h2 to the lower bound the user inputted
+lBound.innerText = lowerBound;
+
+//changes the text of the first h2 to the upper bound the user inputted
+uBound.innerText = upperBound;
+
+const randomNum = (x,y) => {
+    min = Math.ceil(x);
+    max = Math.floor(y);
+     return Math.floor((Math.random()* (max-min+1) + min))
+}
