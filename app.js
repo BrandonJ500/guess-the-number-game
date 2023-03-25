854981c9d53882fa150b1b5a23fa71ab53de0fb0
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-03-25 00:01:25 -0700

    add feature that tells user when they get the correct number

--- app.js
+++ app.js
@@ -14,20 +14,29 @@ lBound.innerText = lowerBound;
 //changes the text of the first h2 to the upper bound the user inputted
 uBound.innerText = upperBound;
 
+//random number generator function, min and max inclusive.
 const randomNum = (x,y) => {
     min = Math.ceil(x);
     max = Math.floor(y);
      return Math.floor((Math.random()* (max-min+1) + min))
 }
+
+//holds the value of the random number
 const ranNumber = randomNum(parseInt(lowerBound), parseInt(upperBound));
+
+//select the form so we can take the value of input
 const form = document.querySelector("#guess");
+
+//selects the h2 that we are going to put the current guess in
+//after each guess
 const guessh2 = document.querySelector("#currentG");
 
+//sets the value of guess to 0
 let guess = 0;
 
-
+//
 const input = document.querySelector("input");
-
+let numOfGuesses = 0;
 
    form.addEventListener('submit', (x)=> {
     x.preventDefault();
